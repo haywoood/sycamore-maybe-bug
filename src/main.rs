5729958406dca779f947f3fbde3bfc12ef0078e1
Ryan Haywood <ryanhaywoodj@gmail.com>
2021-11-01 21:20:09 -0400

    boom

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,135 @@
-use sycamore::{
-    prelude::*,
-    reactive::create_context_scope,
-    context::{ContextProvider, ContextProviderProps, use_context},
-};
-use web_sys::Event;
+use log::debug;
+use log::Level;
+use std::cell::*;
+use std::collections::HashMap;
+use std::rc::*;
+use sycamore::generic_node::*;
+use sycamore::prelude::*;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+
+type PostEventCallback = Box<dyn FnOnce()>;
+
+#[derive(Debug)]
+enum Event {
+    InitializeDB,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Trigger {
+    AddEvent,
+    RunQueue,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum FsmState {
+    Idle,
+    Scheduled,
+    Running,
+    Paused,
+}
 
 #[derive(Clone)]
-struct AppState {
-    count: Signal<i32>,
-    counts: Signal<Vec<Signal<i32>>>,
+struct EventQueue {
+    fsm_state: Rc<RefCell<FsmState>>,
+    queue: Rc<RefCell<Vec<Option<Event>>>>,
+    post_event_callback_fns: Rc<RefCell<HashMap<String, PostEventCallback>>>,
 }
 
-#[component(Count<G>)]
-fn count(c: Signal<i32>) -> Template<G> {
-    let state = use_context::<AppState>();
-    let handle_click = cloned!((state) => move |_event: Event| {
-        create_context_scope(state.clone(), cloned!((state) => move || {
-            let new_count: Vec<Signal<i32>> = state.counts
-                .get()
-                .as_ref()
-                .clone()
-                .into_iter()
-                .chain(Some(Signal::new(0)))
-                .collect();
-            state.counts.set(new_count)
-        }))
-    });
-
-    let handle_click_two = cloned!((state) => move |_event: Event| {
-        state.count.set(state.count.get().as_ref() + 1)
-    });
-
-    template! {
-        p { (c.get()) }
-        p { (state.count.get()) }
-
-        // This will fail with `Uncaught RuntimeError: unreachable executed`
-        button(on:click=handle_click) {
-            "click me"
-        }
-
-        // this works
-        button(on:click=handle_click_two) {
-            "click me 2"
+impl EventQueue {
+    pub fn new() -> Self {
+        EventQueue {
+            fsm_state: Rc::new(RefCell::new(FsmState::Idle)),
+            queue: Rc::new(RefCell::new(vec![])),
+            post_event_callback_fns: Rc::new(RefCell::new(HashMap::new())),
         }
     }
-}
-
-#[component(App<G>)]
-fn app() -> Template<G> {
-    let state = use_context::<AppState>();
-    template! {
-        h1 {"count"}
-        Indexed(IndexedProps {
-            iterable: state.counts.handle(),
-            template: move |count| template! {
-                Count(count)
+    fn fsm_trigger(&self, trigger: Trigger, event: Option<Event>) {
+        debug!("Trigger: {:?}", trigger);
+        debug!("Event: {:?}", event);
+        let (new_fsm_state, action_fn): (FsmState, Option<Box<dyn FnOnce()>>) = {
+            let current_fsm_state = *self.fsm_state.borrow();
+            match (current_fsm_state, trigger) {
+                (FsmState::Idle, Trigger::AddEvent) => (
+                    FsmState::Scheduled,
+                    Some(Box::new(move || {
+                        self.add_event(event);
+                        self.run_next_tick();
+                    })),
+                ),
+                _ => (FsmState::Scheduled, Some(Box::new(|| {}))),
             }
-        })
+        };
+
+        self.fsm_state.replace(new_fsm_state);
+
+        match action_fn {
+            Some(action_fn) => action_fn(),
+            None => (),
+        }
     }
 
+    fn push(&self, event: Event) {
+        self.fsm_trigger(Trigger::AddEvent, Some(event))
+    }
+
+    fn add_event(&self, event: Option<Event>) {
+        self.queue.borrow_mut().push(event)
+    }
+
+    fn next_tick(&self, f: &Closure<dyn FnMut()>) {
+        web_sys::window()
+            .expect("no global `window` exists")
+            .request_animation_frame(f.as_ref().unchecked_ref())
+            .expect("wtf");
+    }
+
+    fn run_next_tick(&self) {
+        let self_cloned = self.clone();
+        let next_tick = Closure::wrap(Box::new(move || {
+            debug!("I'm in the closure");
+            self_cloned.fsm_trigger(Trigger::RunQueue, None)
+        }) as Box<dyn FnMut()>);
+        self.next_tick(&next_tick);
+        // we need to forget lest we want our closure to get dropped.
+        // This can be a source of memory leaks CARE
+        next_tick.forget()
+    }
+}
+
+thread_local! {
+    static EVENT_QUEUE: RefCell<EventQueue> = RefCell::new(EventQueue::new());
+}
+
+fn dispatch(event: Event) {
+    EVENT_QUEUE.with(|event_queue| {
+        event_queue.borrow_mut().push(event);
+    })
 }
 
 fn main() {
-    let app_state = AppState {
-        count: Signal::new(10),
-        counts: Signal::new(vec![Signal::new(0)]),
-    };
+    console_log::init_with_level(Level::Debug);
 
-    sycamore::render(|| template! {
-        ContextProvider(ContextProviderProps {
-            value: app_state,
-            children: || template! {
-                div {
-                    App()
-                }
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let body = &document.body().unwrap();
+
+    dispatch(Event::InitializeDB);
+
+    let x = EventQueue::new();
+    x.fsm_state.replace(FsmState::Scheduled);
+    debug!("x: {:?}", x.fsm_state.borrow());
+    let y = x.clone();
+    y.fsm_state.replace(FsmState::Running);
+    debug!("y: {:?}", y.fsm_state.borrow());
+    debug!("x: {:?}", x.fsm_state.borrow());
+
+    render_plain(
+        || {
+            template! {
+                p { "coca~colas" }
             }
-        })
-    });
+        },
+        body,
+    );
 }
